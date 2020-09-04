126f624756c3cdc25371ad3f2800dbf69e9c893e
prinxard <prinxard@gmail.com>
2020-09-04 01:58:57 +0100

    update

--- task_6.py
+++ task_6.py
@@ -10,7 +10,8 @@ nums = [2, 8, 4, 7, 9, 5, 1]
 def indices_sum(nums, target):
     for i in range(len(nums)):
         for j in range(i + 1, len(nums)):
-            for k in range(i+1, len(nums)):
+            for k in range(i+2, len(nums)):
                 if nums[i]+nums[j]+nums[k] == target:
                     return [nums[i], nums[j], nums[k]]
+                
 print (indices_sum(nums, target))
